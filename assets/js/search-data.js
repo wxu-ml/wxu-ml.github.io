a44b3d72bf44234d35db55ebdd237a5c6346a30f
wxu-ml <150608728+wxu-ml@users.noreply.github.com>
2025-01-11 15:14:44 +0000

    Deploying to gh-pages from @ wxu-ml/wxu-ml.github.io@d29679b759ef62e0abc01d796a5f8251ef182344 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,6 +30,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-services",
+          title: "Services",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/services/";
+          },
         },{id: "nav-teaching",
           title: "teaching",
           description: "Materials for courses you taught. Replace this text with your description.",
